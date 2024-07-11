6452523c29d8afb801df88b8c2e7be62ae39858b
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-07-12 01:57:14 +0530

    side bar and navbar pr working and pages setup krdiye

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,10 +1,42 @@
+'use client' //usepathname only works on client components so we used use client directive at the top of the file to use it. in new nextjs all pages and components and hooks are server side rendered. go through the documentation of client components in next
+
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = () => {
+    const pathName= usePathname();
   return (
-    <div>
-      
-    </div>
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+            <div className='flex flex- flex-col gap-6'>
+
+            {sidebarLinks.map((link)=>{
+                const isActive= pathName=== link.route || pathName.startsWith(link.route);
+                return (
+                    <Link 
+                    href={link.route}
+                    key={link.label}
+                    className={cn('flex gap-4 items-center p-4 rounded-lg justify-start',{
+                        'bg-blue-1':isActive,
+                    })}
+                    >
+                        <Image 
+                        src={link.imgUrl}
+                        alt={link.label}
+                        width={24}
+                        height={24}
+                        />
+                    <p className='text-lg font-semibold max-lg:hidden'>
+                        {link.label}
+                    </p>
+                     </Link>
+                )
+            })}
+            </div>
+    </section>
   )
 }
 
