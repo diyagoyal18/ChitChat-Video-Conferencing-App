4f893eda1d8e0f037f7fd55aff34767bab894fcc
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-07-12 23:22:25 +0530

    navbar and basic design

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,20 +1,25 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
         <Link href="/" className='flex items-center gap-1'>
         <Image 
         src="icons/logo.svg"
-        alt='Logo'
+        alt=' Chitchat Logo'
         width={32}
         height={32}
         className='max-sm:size-10'
         />
-        <p className='text-[26px] font-extrabold max-sm:hidden text-white'>Yapster</p>
+        <p className='text-[26px] font-extrabold max-sm:hidden text-white'>ChitChat</p>
         </Link>
+        <div className='flex-between gap-5'>
+
+            <MobileNav/>
+        </div>
     </nav>
   )
 }
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -11,10 +11,10 @@ const Sidebar = () => {
     const pathName= usePathname();
   return (
     <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
-            <div className='flex flex- flex-col gap-6'>
+            <div className='flex flex-1 flex-col gap-6'>
 
             {sidebarLinks.map((link)=>{
-                const isActive= pathName=== link.route || pathName.startsWith(link.route);
+                const isActive= pathName=== link.route || pathName.startsWith(`${link.route}/`) ;
                 return (
                     <Link 
                     href={link.route}
