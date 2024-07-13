393e4836121ed44dbb2c31b71c69e3f6e8914a2b
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-07-14 01:20:54 +0530

    meeting modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import HomeCard from "./HomeCard"
 import { useRouter } from "next/navigation"
 import NextLink from 'next/link';
+import MeetingModal from "./MeetingModal";
 
 
 const MeetingTypeList = () => {
@@ -11,6 +12,9 @@ const router= useRouter();
     const [meetingState, setMeetingState] = useState<
       'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
     >(undefined);
+    const createMeeting = () =>{
+
+    }
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4 '>
        <HomeCard 
@@ -42,6 +46,15 @@ const router= useRouter();
         className="bg-pink-4"
         handleClick={() => router.push('/recordings')}
       />
+        <MeetingModal
+         isOpen= {meetingState==='isInstantMeeting'}
+         onClose={()=>setMeetingState(undefined)}
+         title= "Start an Instant Meeting"
+         className="text-center"
+         buttonText="Start Meeting"
+         handleClick={createMeeting}
+        />
+
     </section>
   )
 }
