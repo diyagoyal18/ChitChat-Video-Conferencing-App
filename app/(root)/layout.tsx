505ea55483190d73dcdabd00396aab50f01e1747
Diya Goyal <dgoyal6_be22@thapar.edu>
2024-07-14 19:55:17 +0530

    stream se meeting create hogyi

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,13 @@
+import StreamVideoProvider from '@/providers/streamClientProvider'
 import React, { Children, ReactNode } from 'react'
 
 const RootLayout = ({children}: {children : ReactNode}) => {
   return (
     <main>
+      <StreamVideoProvider>
+
         {children}
+      </StreamVideoProvider>
     </main>
   )
 }
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,6 +5,8 @@ import HomeCard from "./HomeCard"
 import { useRouter } from "next/navigation"
 import NextLink from 'next/link';
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
 
 const MeetingTypeList = () => {
@@ -12,8 +14,43 @@ const router= useRouter();
     const [meetingState, setMeetingState] = useState<
       'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
     >(undefined);
-    const createMeeting = () =>{
+    const {user} = useUser();
+    const client = useStreamVideoClient();
+    const [values, setValues] = useState({
+      dateTime: new Date(),
+      description: '',
+      link: ''
+    });
+    const [callDetails, setCallDetails] = useState<Call>()
+    const createMeeting = async () =>{
+      if(!client || !user) return;
 
+      try {
+        const id= crypto.randomUUID();
+        const call= client.call('default',id);
+
+        if(!call) throw new Error('Failed to Create a call')
+
+          const startsAt =  values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+          const description = values.description || 'Instant Meeting';
+
+          await call.getOrCreate({
+            data:{
+              starts_at: startsAt,
+              custom:{
+                description
+              }
+            }
+          })
+          setCallDetails(call);
+
+          if(!values.description){
+            router.push(`/meeting/${call.id}`)
+          }
+
+      } catch (error) {
+        console.log(error);
+      }
     }
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4 '>
